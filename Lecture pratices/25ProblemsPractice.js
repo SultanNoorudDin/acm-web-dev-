997e3cef2e29d7f8c162e72e371a86fb85a39e8a
SultanNoorudDin <sultannooruddin4@gmail.com>
2023-07-27 11:27:50 +0500

    last for this week

---
+++ Lecture pratices/25ProblemsPractice.js
@@ -0,0 +1,77 @@
+// 24 problems actually
+
+// 1
+
+// function multiply(a, b){
+//     return a * b
+//   }
+
+// ------------------------------------------
+
+
+// 2
+
+// function even_or_odd(number) {
+//     if (number%2 == 0) {
+//       return "Even";
+//     } else {
+//       return "Odd";
+//     }
+//   }
+
+// ---------------------------------------------
+
+// 3
+
+
+// function countSheeps(arrayOfSheep) {
+//     // TODO May the force be with you
+//     var num = 0;
+    
+//     for(var i = 0; i < arrayOfSheep.length; i++)
+//       if(arrayOfSheep[i] == true)
+//         num++;
+        
+//     return num;
+//   }
+
+// --------------------------------------------
+
+// 4
+
+// function isDivisible(n, x, y) {
+//     if(n%x == 0 && n%y == 0) return true
+//     else return false
+//   }
+
+//-------------------------------------------------
+// 5
+
+// function makeNegative(num) {
+//     // Code?
+//     if (num < 0) return num
+//     else return num*-1;
+//   }
+
+//-------------------------------------------------
+// 6
+// class SmallestIntegerFinder {
+//     findSmallestInt(args) {
+//       return Math.min(...args)
+//     }
+//   }
+
+// ---------------------------------------------
+
+// 7 
+// var summation = function (num) {
+//     // Code here
+//     let sum = 0;
+//     for (let i=0; i<=num;i++){
+//       sum += i;
+//     }
+//     return sum;
+//   }
+
+// 8
+
